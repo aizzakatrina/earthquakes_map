a8c1cdd99f723819ab61ae6fb3bae5584cc6bf01
aizzakatrina <aizzakatrina@live.com>
2018-03-31 23:46:22 -0700

    Added fault line layer

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-// Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Store our API and data endpoints insides variables
+var earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var faultLineURL = "data/PB2002_boundaries.json";
 
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-    // Receive a response and send the data.features object to the createFeatures function
-    createFeatures(data.features);
+// json read of fault line data
+d3.json(faultLineURL, function(data) {
+    // Receive a response and send the data.features object to the createFaultLines function
+    createFaultLines(data.features);
 });
 
-function createFeatures(earthquakeData) {
+// Perform a GET request to the query URL for earthquake data
+d3.json(earthquakeURL, function(data) {
+    // Receive a response and send the data.features object to the createEarthquakeMarkers function
+    createEarthquakeMarkers(data.features);
+});
+
+function createFaultLines(faultLineData) { 
+    // Define a function to make fault lines
+    function makeFaultLines (features, latlng) {
+        var faultLine = {
+            color: "blue",
+            weight: 3,
+        };
+        return L.polyline(latlng, faultLine);
+    };
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing details of the fault line
+    function onEachFeature(feature, layer) {
+        var faultContent = "<h3>Name: " + feature.properties.Name + "</h3><hr>Coordiantes: (" + feature.geometry.coordinates[0][1]  + "," + feature.geometry.coordinates[0][0] + ")";
+        layer.bindPopup(faultContent).addTo(faultLines);
+    };
+    // Create a GeoJSON layer containing the features array on the faultLineData object
+    // Run the onEachFeature function once for each piece of data in the array
+    L.geoJSON(faultLineData, {
+        pointToLayer: makeFaultLines,
+        onEachFeature: onEachFeature
+        }
+    );
+};
+
+var faultLines = new L.LayerGroup();
+
+function createEarthquakeMarkers(earthquakeData) {
     // Define a function to make markers
     function markCircle(feature, latlng) {
         var mag = feature.properties.mag;
@@ -21,12 +54,10 @@ function createFeatures(earthquakeData) {
         };
         return L.circleMarker(latlng, MarkerStyle);
     };
-
     // Define a markerSize function that will give each marker a different radius based on its magnitude
     function markerSize(magnitude) {
         return (magnitude * 5);
     };
-
     // Define a function that will give each marker a different color based on its magnitude
     function markerColor(fill) {
         switch (true) {
@@ -50,15 +81,13 @@ function createFeatures(earthquakeData) {
                 break;
         }
         return color;
-    };
-    
+    };    
     // Define a function we want to run once for each feature in the features array
     // Give each feature a popup describing details of the earthquake
     function handleFeature(feature, layer) {
-        layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>Magnitude: " + feature.properties.mag + 
-        "<br>" + new Date(feature.properties.time) + "</p>");
+        var earthquakeContent = "<h3>" + feature.properties.place + "</h3><hr>Magnitude: " + feature.properties.mag + "<br>" + new Date(feature.properties.time);
+        layer.bindPopup(earthquakeContent);
     };
-
     // Create a GeoJSON layer containing the features array on the earthquakeData object
     // Run the handleFeature function once for each piece of data in the array
     var earthquakes = L.geoJSON(earthquakeData, {
@@ -66,7 +95,6 @@ function createFeatures(earthquakeData) {
         onEachFeature: handleFeature
         }
     );
-
     // Send earthquakes layer to the createMap function
     createMap(earthquakes);
 };
@@ -90,13 +118,14 @@ function createMap(earthquakes) {
     };
     // Create overlay object to hold overlay layer
     var overlayMaps = {
-        Earthquakes: earthquakes
+        "Earthquakes": earthquakes,
+        "Fault Lines": faultLines
     };
     // Create map with layers to display on load
     var myMap = L.map("map", {
         center: [37.09, -95.71],
         zoom: 3,
-        layers: [streetmap, earthquakes]
+        layers: [streetmap, earthquakes, faultLines]
     });
     // Create a layer control, pass the baseMaps and overlayMaps, and add the layer control to the map
     L.control
@@ -107,15 +136,14 @@ function createMap(earthquakes) {
     var legend = L.control({position: 'bottomright'});
     legend.onAdd = function (map) {
         // Creates a div with class="info legend"
-        var div = L.DomUtil.create('div', 'info legend');
+        var div = L.DomUtil.create('div', 'info legend'),
+            labels = ['0-1','1-2','2-3','3-4','4-5','5+'],
+            colors = ['99CC00','CCFF33','FFCC00','FF9900','FF5050','FF0000'];
         // Sets the html code inside the div
         div.innerHTML = 'Magnitude<hr>';
-        div.innerHTML += '<table><tr><td style="background-color: #99CC00;">&nbsp;</td><td>0-1</td></tr></table>';
-        div.innerHTML += '<table><tr><td style="background-color: #CCFF33;">&nbsp;</td><td>1-2</td></tr></table>';
-        div.innerHTML += '<table><tr><td style="background-color: #FFCC00;">&nbsp;</td><td>2-3</td></tr></table>';
-        div.innerHTML += '<table><tr><td style="background-color: #FF9900;">&nbsp;</td><td>3-4</td></tr></table>';
-        div.innerHTML += '<table><tr><td style="background-color: #FF5050;">&nbsp;</td><td>4-5</td></tr></table>';
-        div.innerHTML += '<table><tr><td style="background-color: #FF0000;;">&nbsp;</td><td>5+</td></tr></table>';
+        for (var i=0; i<labels.length; i++){
+            div.innerHTML += '<table><tr><td style="background-color: #' + colors[i] + ';">&nbsp;</td><td>' + labels[i] + '</td></tr></table>';
+        };
         return div;
     };
     // Add legend to myMap
